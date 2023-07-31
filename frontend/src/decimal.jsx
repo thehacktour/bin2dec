981c458e6b4f88c703cc26cda14a941fa09f0535
Atilio <atilio.jesus@grupoboticario.com.br>
2023-07-30 21:51:56 -0300

    Add binary to decimal function

--- frontend/src/decimal.jsx
+++ frontend/src/decimal.jsx
@@ -1,13 +1,42 @@
 import { useState } from 'react'
 
-const binaryToDecimal = (binary) => {
+const binaryToDecimal = (binaryNumber) => {
+  // Verifica se a entrada é um número binário válido (contendo apenas 0 e 1)
+  const binaryRegex = /^[0-1]+$/;
+  if (!binaryRegex.test(binaryNumber)) {
+    return "Entrada inválida! Digite apenas 0 e 1.";
+  }
 
+  // Converte o número binário para decimal
+  const decimalNumber = parseInt(binaryNumber, 2);
+  return decimalNumber;
 };
-
 const GeradorDeDecimal = () => {
+
+  const [binarioInicial, aparecerNaTela] = useState('');
+  const [binarioIncremento, aparecerIncremento] = useState('');
+
+  const manipuladorDeDecimal = (event) => {
+    const valor = event.target.value;
+    aparecerNaTela(valor);
+    aparecerIncremento(binaryToDecimal(valor));
+  }
+
+  const deletarBinario = () =>{
+    aparecerIncremento('');
+    aparecerNaTela('');
+  }
+
   return (
     <div>
-
+      <h2>Escreva seu Numero Binario:</h2>
+      <input
+        type="number"
+        value={binarioInicial}
+        onChange={manipuladorDeDecimal}
+      />
+      <h2>Resultado: {binarioIncremento}</h2>
+      <button onClick={deletarBinario}>Apagar</button>
     </div>
   );
 }
