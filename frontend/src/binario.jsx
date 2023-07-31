5e7ae9f1b2ee678276d3aa574fc29ce0d188feed
Atilio <atilio.jesus@grupoboticario.com.br>
2023-07-30 21:37:49 -0300

    IntegerToBinary function done first part

--- frontend/src/binario.jsx
+++ frontend/src/binario.jsx
@@ -5,10 +5,26 @@ const integerToBinary = (number) => {
 };
 
 const GeradorDeBinario = () => {
-  
+
+  const [decimalPreFuncao, aparecerNaTela] = useState('');
+  const [decimalComFuncao, aplicarFuncao] = useState('');
+
+  const acontecerNaTela = (event) => {
+    const valor = event.target.value;
+    aparecerNaTela(valor);
+    aplicarFuncao(integerToBinary(parseInt(valor, 10) || 0));
+  }
+
   return (
     <div>
+      <h2>Escreva seu Numero Decimal: </h2>
+      <input
+        type="number"
+        value={decimalPreFuncao}
+        onChange={acontecerNaTela}
+      />
 
+      <h2>Resultado: {decimalComFuncao}</h2>
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import GeradorDeDecimal from './decimal.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1>Tradutor de Binario e Decimal!</h1>
     <GeradorDeBinario/>
     <GeradorDeDecimal/>
   </React.StrictMode>,
